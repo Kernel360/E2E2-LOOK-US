b16cc1521cd7609aa1b13b01f1ae75c7273cc21e
limbaba1120 <kwlim1120ss@gmail.com>
2024-07-30 11:26:10 +0900

    feat: [#1] 프로젝트 초기 설정 추가 및 변경
    - 폴더 구조 변경

---
+++ backend/src/main/java/org/example/LookusApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookusApplication.class, args);
+	}
+
+}

8eb98713fd62f22a3df8954c589b151c503200b1
kimminkyeu <minkyeki@student.42seoul.kr>
2024-07-31 15:32:33 +0900

    :art: feat(#14): 생성시간 / 수정시간 자동 엔티티 설정 기능 (상속)

--- backend/src/main/java/org/example/common/TimeTrackableEntity.java
+++ backend/src/main/java/org/example/common/TimeTrackableEntity.java
@@ -1,5 +1,7 @@
 package org.example.common;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -7,8 +9,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-
-import java.time.LocalDateTime;
 
 import lombok.Getter;
 

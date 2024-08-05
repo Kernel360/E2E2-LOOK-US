cb391835b2482f9f6d09c24c72a12166fb67a3a6
limbaba1120 <kwlim1120ss@gmail.com>
2024-08-05 18:00:33 +0900

    fix: [#52] merge 충돌 해결

--- backend/src/main/java/org/example/post/domain/dto/PostDto.java
+++ backend/src/main/java/org/example/post/domain/dto/PostDto.java
@@ -3,13 +3,11 @@ package org.example.post.domain.dto;
 import java.util.List;
 
 import org.example.post.domain.entity.PostEntity;
-import org.springframework.web.multipart.MultipartFile;
 
 public class PostDto {
 	public record CreatePostDtoRequest(
 		String postContent,
-		List<String> hashtagContents,
-		MultipartFile image
+		List<String> hashtagContents
 	) {
 	}
 

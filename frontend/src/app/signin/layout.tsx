137933e3a692c4b5c5fae7d057e457a9865a4f76
Minky <minkyeki@student.42seoul.kr>
2024-08-09 03:04:00 +0900

    Feat/front 회원가입 UI 구현 (#78)
    * feat: frontend login
    
    * feat: frontend login
    
    * feat: http://localhost:3000으로 redirect 경로 변
    
    ---------
    
    Co-authored-by: Minju Kim <49716188+Minju-Kimm@users.noreply.github.com>

--- frontend/src/app/signin/layout.tsx
+++ frontend/src/app/signin/layout.tsx
@@ -34,24 +34,7 @@ export default function ExamplesLayout({ children }: ExamplesLayoutProps) {
               Dashboard, cards, authentication. Some examples built using the
               components. Use this as a guide to build your own.
             </PageHeaderDescription>
-            <PageActions>
-              <Link
-                href="/docs"
-                className={cn(buttonVariants(), "rounded-[6px]")}
-              >
-                Get Started
-              </Link>
-              <Link
-                href="/components"
-                className={cn(
-                  buttonVariants({ variant: "outline" }),
-                  "rounded-[6px]"
-                )}
-              >
-                Components
-              </Link>
-            </PageActions>
-          </PageHeader>
+            </PageHeader>
           <section>
             <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl">
               {children}

459e72c3148d21c287d04d02d2c5c156a8cc46b9
kimminkyeu <minkyeki@student.42seoul.kr>
2024-08-11 19:45:25 +0900

    :wrench: refactor: image public api 경로 수정 (/api/a1)

--- backend/src/main/java/org/example/image/controller/ImagePublicResourceController.java
+++ backend/src/main/java/org/example/image/controller/ImagePublicResourceController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  * </li>
  */
 @RestController
+@RequestMapping("/api/a1")
 public class ImagePublicResourceController {
 
 	private final ImageStorageManager imageStorageManager;

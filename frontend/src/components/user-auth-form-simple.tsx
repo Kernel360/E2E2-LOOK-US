0e97c95fdc05cbf76a9a352a0ccd00b10f52eee8
Minky <minkyeki@student.42seoul.kr>
2024-08-11 21:07:06 +0900

    이미지 컨트롤러 API 일괄 수정 및 프론트 반영  (#92)
    * :recycle: OAUTH 용 API 경로 추가
    
    * :recycle: OAUTH 용 API 경로 추가
    
    * :recycle: image public api 경로 /api/a1 으로 일괄 업데이트
    
    * :recycle: image aspect ratio 상수 설정 파일로 분리
    
    * :recycle: image x버튼 클릭 시 제거 안되는 버그 수정
    
    * package.lock.json 임시 제거
    
    * package.lock.json 임시 제거

--- frontend/src/components/user-auth-form-simple.tsx
+++ frontend/src/components/user-auth-form-simple.tsx
@@ -1,29 +1,27 @@
-"use client"
+'use client'
 
-import * as React from "react"
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
-import { Input } from "./ui/input"
-import { Label } from "./ui/label"
-import { Icons } from "./Icons"
-import Link from "next/link"
-import { buttonVariants } from "./ui/button"
-import { API_PUBLIC_URL } from "@/app/_common/constants"
-
+import { cn } from '@/lib/utils'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { Icons } from './Icons'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+import { API_OAUTH2_URL, API_PUBLIC_URL } from '@/app/_common/constants'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-
-  return (
-    <div className={cn("grid gap-6", className)} {...props}>
-          <Link
-            href={`${API_PUBLIC_URL}/oauth2/authorization/google`}
-            className={cn(buttonVariants(), "rounded-[6px]")}
-          >
-          <Icons.google className="mr-2 h-4 w-4" />
-           Google
-          </Link>
-    </div>
-  )
+    return (
+        <div className={cn('grid gap-6', className)} {...props}>
+            <Link
+                href={`${API_OAUTH2_URL}/google`}
+                className={cn(buttonVariants(), 'rounded-[6px]')}
+            >
+                <Icons.google className='mr-2 h-4 w-4' />
+                Google
+            </Link>
+        </div>
+    )
 }

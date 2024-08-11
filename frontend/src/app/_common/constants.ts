1dacb96f36abfef9b73c17296443e4dc4b6a214e
kimminkyeu <minkyeki@student.42seoul.kr>
2024-08-11 21:01:06 +0900

    :recycle: image aspect ratio 상수 설정 파일로 분리

--- frontend/src/app/_common/constants.ts
+++ frontend/src/app/_common/constants.ts
@@ -4,3 +4,8 @@ const SERVER_IP_ADDR = "http://localhost:8080"
 export const API_PRIVATE_URL = `${SERVER_IP_ADDR}/api/v1`
 export const API_PUBLIC_URL = `${SERVER_IP_ADDR}/api/a1`;
 export const API_OAUTH2_URL = `${SERVER_IP_ADDR}/oauth2/authorization`;
+
+/**
+ * TODO: Set appropriate image ratio for fassion styleshot.
+ */
+export const IMAGE_ASPECT_RATIO = 2 / 3

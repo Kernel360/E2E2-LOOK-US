c3a6b496df871dd643ee081bf1c24a8cd11acc01
kimminkyeu <minkyeki@student.42seoul.kr>
2024-08-11 20:08:54 +0900

    :recycle: OAUTH 용 API 경로 추가

--- frontend/src/app/_common/constants.ts
+++ frontend/src/app/_common/constants.ts
@@ -1,3 +1,6 @@
 
-export const API_PRIVATE_URL = "http://localhost:8080/api/v1"
-export const API_PUBLIC_URL = "http://localhost:8080/api/a1";
+const SERVER_IP_ADDR = "http://localhost:8080"
+
+export const API_PRIVATE_URL = `${SERVER_IP_ADDR}/api/v1`
+export const API_PUBLIC_URL = `${SERVER_IP_ADDR}/api/a1`;
+export const API_OAUTH2_URL = `${SERVER_IP_ADDR}/oauth2/authorization`;

71783a22795eb6bfa8ef00ec19c73236fe3285f2
kimminkyeu <minkyeki@student.42seoul.kr>
2024-08-13 16:37:28 +0900

    :recycle: Follow 테이블 이름 변경

--- backend/src/main/java/org/example/follow/domain/entity/Follow.java
+++ backend/src/main/java/org/example/follow/domain/entity/Follow.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Table(name = "follow")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder

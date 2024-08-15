f07c0ee99609d5143d1a5bb6245688e8170c2f03
kimminkyeu <minkyeki@student.42seoul.kr>
2024-08-15 17:14:29 +0900

    refactor: 미사용 import 제거

--- backend/src/main/java/org/example/post/controller/PostApiController.java
+++ backend/src/main/java/org/example/post/controller/PostApiController.java
@@ -2,13 +2,11 @@ package org.example.post.controller;
 
 import org.example.exception.common.ApiErrorCategory;
 
-import org.example.exception.user.ApiUserErrorSubCategory;
-import org.example.exception.user.ApiUserException;
+import org.example.exception.post.ApiPostErrorSubCategory;
+import org.example.exception.post.ApiPostException;
 
 import org.example.post.domain.dto.PostDto;
 import org.example.post.service.PostService;
-import org.example.user.domain.entity.member.UserEntity;
-import org.example.util.ClassUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;

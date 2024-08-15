737af9ab7cc407cee1ad49676816356a40d8f191
kimminkyeu <minkyeki@student.42seoul.kr>
2024-08-15 17:16:20 +0900

    refactor: User 서비스 코드 리팩토링

--- backend/src/main/java/org/example/user/service/member/UserService.java
+++ backend/src/main/java/org/example/user/service/member/UserService.java
@@ -38,10 +38,11 @@ public class UserService {
 		String email,
 		MultipartFile profileImage
 	) {
-		UserEntity user = this.getUser(email);
+		UserEntity user = this.getUserByEmail(email);
+		AssertThat_UserAccountIsActive(user);
 
-		ThrowApiExceptionIfUserAccountIsDeactivated(user);
-
+		// TODO: image storage는 ApiException이 아닌 자체적 Exception을 던지도록
+		//       변경될 예정입니다. 후추 try-catch로 변경해야 합니다.
 		if (profileImage != null && !profileImage.isEmpty()) {
 			StorageSaveResult storageSaveResult = imageStorageManager.saveResource(
 				profileImage, StorageType.LOCAL_FILE_SYSTEM
@@ -51,93 +52,119 @@ public class UserService {
 		}
 
 		if(updateRequest != null) {
-			user.updateDetails(updateRequest.birth(), updateRequest.instaId(), updateRequest.nickName(),
-				updateRequest.gender());
+			user.updateDetails(
+				updateRequest.birth(),
+				updateRequest.instaId(),
+				updateRequest.nickName(),
+				updateRequest.gender()
+			);
 		}
 
 		return UserDto.UserUpdateResponse.toDto(user);
 	}
 
-	public Long saveUser(UserDto.UserCreateRequest addUserRequest) {
-		UserEntity user = this.getUser(addUserRequest.email());
+	public void signupUser(UserDto.UserCreateRequest addUserRequest) {
+		UserEntity user = this.getUserByEmail_internal(addUserRequest.email());
 
+		// 이미 탈퇴한 사용자가 기존 이메일로 재가입 할 경우를 처리합니다
 		if (user.getUserStatus() == UserStatus.USER_STATUS_DEACTIVATE) {
-			user.setUserStatus(UserStatus.USER_STATUS_ACTIVATE);
+			user.updateUserStatus(UserStatus.USER_STATUS_ACTIVATE);
 			userRepository.save(user);
 
-			return user.getUserId();
+			return;
 		}
 
-		return userRepository.save(UserEntity.builder()
+		userRepository.save(UserEntity.builder()
 			.email(addUserRequest.email())
 			.password(bCryptPasswordEncoder.encode(addUserRequest.password()))
-			.build()).getUserId();
+			.build());
 	}
 
 	@Transactional(readOnly = true)
 	public UserDto.UserGetInfoResponse getMyInfo(String email) {
-		UserEntity user = this.getUser(email);
-
-		ThrowApiExceptionIfUserAccountIsDeactivated(user);
+		UserEntity user = this.getUserByEmail_internal(email);
+		AssertThat_UserAccountIsActive(user);
 
 		return UserDto.UserGetInfoResponse.toDto(user,  userRepository.postCount(user));
 	}
 
 	@Transactional(readOnly = true)
-	public UserEntity findById(Long userId) {
-		UserEntity user = userRepository.findById(userId)
-			.orElseThrow(() -> ApiUserException.builder()
-				.category(ApiErrorCategory.RESOURCE_BAD_REQUEST)
-				.subCategory(ApiUserErrorSubCategory.USER_NOT_FOUND)
-				.setErrorData(() -> "찾을 수 없는 유저입니다.")
-				.build());
-
-		ThrowApiExceptionIfUserAccountIsDeactivated(user);
-
-		return user;
-	}
-
-	@Transactional(readOnly = true)
-	public UserEntity findByEmail(String email) {
-		UserEntity user = this.getUser(email);
-
-		ThrowApiExceptionIfUserAccountIsDeactivated(user);
-
-		return user;
-	}
-
-	@Transactional(readOnly = true)
 	public List<UserDto.UserGetPostsResponse> getMyPosts(String email) {
-		UserEntity user = this.getUser(email);
+		UserEntity user = this.getUserByEmail_internal(email);
+		AssertThat_UserAccountIsActive(user);
 
 		return userRepository.postList(user);
 	}
 
 	public void resignUser(String email, HttpServletRequest request, HttpServletResponse response) {
-		UserEntity user = findByEmail(email);
-		user.setUserStatus(UserStatus.USER_STATUS_DEACTIVATE);
+		// 이미 탈퇴한 사용자에 대한 탈퇴 요청은 비정상 Request 입니다.
+		UserEntity user = this.getUserByEmail_internal(email);
+		AssertThat_UserAccountIsActive(user);
 
+		user.updateUserStatus(UserStatus.USER_STATUS_DEACTIVATE);
+		this.logoutUser(email, request, response);
+	}
+
+	public void logoutUser(String email, HttpServletRequest request, HttpServletResponse response) {
+		// 이미 탈퇴한 사용자에 대한 로그아웃 요청은 비정상 Request 입니다.
+		UserEntity user = this.getUserByEmail_internal(email);
+		AssertThat_UserAccountIsActive(user);
+
+		// 로그-아웃 처리를 위해, 클라이언트 브라우저에서 토큰 쿠키를 삭제합니다.
 		CookieUtil.deleteCookie(request, response, ACCESS_TOKEN_COOKIE_NAME);
 		CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
 	}
 
+	/**
+	 * NOTE: 만약 비활성화된 사용자를 검색한 경우, ApiUserException을 발생시킵니다.
+	 */
+	public UserEntity getUserById(Long userId) throws ApiUserException {
+		UserEntity user = getUserById_internal(userId);
+		AssertThat_UserAccountIsActive(user);
 
-	private UserEntity getUser(String email) throws ApiUserException {
+		return user;
+	}
+
+	/**
+	 * NOTE: 만약 비활성화된 사용자를 검색한 경우, ApiUserException을 발생시킵니다.
+	 */
+	public UserEntity getUserByEmail(String email) throws ApiUserException {
+		UserEntity user = getUserByEmail_internal(email);
+		AssertThat_UserAccountIsActive(user);
+
+		return user;
+	}
+
+	// Private Helper Methods ----------------------------------------------------
+
+	/**
+	 * NOTE: DEACTIVATED 처리된 사용자도 검색에 포함됩니다.
+	 */
+	private UserEntity getUserById_internal(Long userId) throws ApiUserException {
+		return userRepository.findById(userId)
+							 .orElseThrow(() -> ApiUserException.builder()
+								 .category(ApiErrorCategory.RESOURCE_BAD_REQUEST)
+								 .subCategory(ApiUserErrorSubCategory.USER_NOT_FOUND)
+								 .build());
+	}
+
+	/**
+	 * NOTE: DEACTIVATED 처리된 사용자도 검색에 포함됩니다.
+	 */
+	private UserEntity getUserByEmail_internal(String email) throws ApiUserException {
 		return userRepository.findByEmail(email)
-							 .orElseThrow(
-								 () -> ApiUserException.builder()
-									 .category(ApiErrorCategory.RESOURCE_INACCESSIBLE)
-									 .subCategory(ApiUserErrorSubCategory.USER_NOT_FOUND)
-									 .build()
+							 .orElseThrow(() -> ApiUserException.builder()
+								 .category(ApiErrorCategory.RESOURCE_BAD_REQUEST)
+								 .subCategory(ApiUserErrorSubCategory.USER_NOT_FOUND)
+								 .build()
 							 );
 	}
 
-	private void ThrowApiExceptionIfUserAccountIsDeactivated(UserEntity userEntity) throws ApiUserException {
+	private void AssertThat_UserAccountIsActive(UserEntity userEntity) throws ApiUserException {
 		if (userEntity.getUserStatus() == UserStatus.USER_STATUS_DEACTIVATE) {
 			throw ApiUserException.builder()
 				.category(ApiErrorCategory.RESOURCE_INACCESSIBLE)
 				.subCategory(ApiUserErrorSubCategory.USER_DEACTIVATE)
-				.setErrorData(() -> ("입력된 이메일을 다시 확인하세요" + userEntity.getEmail()))
 				.build();
 		}
 	}

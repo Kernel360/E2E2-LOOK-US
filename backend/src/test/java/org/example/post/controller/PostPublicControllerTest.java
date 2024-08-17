2194046aedce009853c2c0619ab57f15ceecdbec
Tigris <kyrae604@naver.com>
2024-08-17 18:28:35 +0900

    [Feat] RGB 변환기, Base64 인코더 구현

--- backend/src/test/java/org/example/post/controller/PostPublicControllerTest.java
+++ backend/src/test/java/org/example/post/controller/PostPublicControllerTest.java
@@ -3,23 +3,31 @@ package org.example.post.controller;
 import org.example.post.domain.dto.PostDto;
 import org.example.post.repository.custom.PostSearchCondition;
 import org.example.post.service.PostService;
+import org.example.user.controller.member.UserApiController;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@ExtendWith({SpringExtension.class, MockitoExtension.class}) // Spring Context 로드, Mockito를 사용해서 Mock 객체 주입
+@WebMvcTest(UserApiController.class) // Spring MVC테스트를 위한 설정
 public class PostPublicControllerTest {
 
 	@Mock
@@ -34,7 +42,8 @@ public class PostPublicControllerTest {
 	}
 
 	@Test
-	void searchPost_shouldReturnPosts() {
+	@DisplayName("모든 게시글 조회 findAllPosts 정상 리턴 확인 테스트")
+	void searchPost_shouldReturnPostPage() {
 		// Arrange
 		PostSearchCondition postSearchCondition = new PostSearchCondition();
 		Pageable pageable = PageRequest.of(0, 10);
@@ -51,6 +60,7 @@ public class PostPublicControllerTest {
 	}
 
 	@Test
+	@DisplayName("Post Id 기반 게시글 조회 getPostById 정상 리턴 확인 테스트")
 	void getPostById_shouldReturnPost() {
 		// Arrange
 		Long postId = 1L;
